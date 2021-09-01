6eef5b7f5a4922fda1457abec788635454774a2e
jiwon615 <seg615@naver.com>
2021-09-01 11:19:33 +0900

    modify: make default master branch

--- src/main/java/com/jiwon/springapplicationcontext/scope/AppRunner.java
+++ src/main/java/com/jiwon/springapplicationcontext/scope/AppRunner.java
@@ -1,25 +1,14 @@
 package com.jiwon.springapplicationcontext.scope;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    MessageSource messageSource;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        while (true) {
-            System.out.println(messageSource.getMessage("greeting", new String[]{"lina"}, Locale.KOREA));
-            System.out.println(messageSource.getMessage("greetings", new String[]{"lina"}, Locale.getDefault()));
-            Thread.sleep(1000l);
-        }
+
     }
 }
